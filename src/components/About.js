e8bb89d8392c71971b5bad34b6ea255ba2e2083f
hardikbachhan <50964127+hardikbachhan@users.noreply.github.com>
2022-01-23 01:13:52 +0530

    add Home.js, About.js, Navbar.js components

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+  return <div>
+      <div className="container">
+          <h1>This is about page.</h1>
+      </div>
+  </div>;
+};
+
+export default About;
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return <div>
+      <div className="container">
+          <h1>This is Home.</h1>
+      </div>
+  </div>;
+};
+
+export default Home;
